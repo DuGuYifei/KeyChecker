9892f85613097dd47d13db9f97aa3d9985797511
DuGuYifei <liuyifei97@163.com>
2023-10-07 16:52:15 +0200

    prod version

---
+++ src/main/resources/public/app.js
@@ -0,0 +1,10 @@
+window.eruptSiteConfig = {
+    //标题
+    title: "AnyHelper 密钥管理系统",
+    //描述
+    desc: "密钥管理系统",
+    //是否展示版权信息
+    copyright: false,
+    logoPath: "android-chrome-512x512.png",
+    logoText: "Full Geek"
+};
